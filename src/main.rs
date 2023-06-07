1dadad1593eac79b8ef765310a31bb174f716aa0
Jibao Mansaray <jibaomansaray@gmail.com>
2023-06-08 03:30:04 +1000

    Add threads

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
+use std::{sync::OnceLock, thread};
+
 use crate::{helpers::inject_service, service_container::ServiceContainerBuilder};
 mod handlers;
 mod helpers;
 mod injectables;
 mod service;
 mod service_container;
+use injectables::Injectable;
 use service::Service;
 
 fn main() {
@@ -13,6 +16,7 @@ fn main() {
             name: "Jibao".into(),
         }))
         .register(Service::new(Grade { id: 5000 }))
+        .register(Service::new(LoggedInUser::new()))
         .build();
 
     // Inject the User instance and call the closure
@@ -35,6 +39,46 @@ fn main() {
             grade.id, &user.name
         );
     });
+
+    inject_service(|logged_in: Service<LoggedInUser>| {
+        dbg!(&logged_in.user());
+    });
+
+    let handle = thread::spawn(|| {
+        inject_service(|logged_in: Service<LoggedInUser>| {
+            dbg!(&logged_in.user());
+        });
+    });
+
+    handle.join().unwrap();
+}
+
+struct LoggedInUser {
+    user: OnceLock<User>,
+}
+
+impl LoggedInUser {
+    pub fn new() -> Self {
+        Self {
+            user: OnceLock::new(),
+        }
+    }
+
+    pub fn user(&self) -> &User {
+        return self.user.get_or_init(|| {
+            println!("creating user for the first time");
+            User {
+                id: 20000,
+                name: "Logged in User".into(),
+            }
+        });
+    }
+}
+
+impl Injectable for LoggedInUser {
+    fn inject(_container: &service_container::ServiceContainer) -> Self {
+        Self::new()
+    }
 }
 
 #[derive(Debug)]
