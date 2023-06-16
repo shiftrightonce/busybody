16a649dd4f7c5f6e047c89532ee5c421d7157828
Shift Right Once <jibaomansaray@gmail.com>
2023-06-16 12:42:42 +1000

    Add a third example for inject_and_call
    Fixed out messages the second inject_and_call example

--- examples/basic3-inject-and-call.rs
+++ examples/basic3-inject-and-call.rs
@@ -1,20 +1,25 @@
 #![allow(dead_code)]
-use busybody::{helpers, Injectable, Service, ServiceContainerBuilder};
+use busybody::{helpers, Injectable, ServiceContainerBuilder, Singleton};
 
 #[tokio::main]
 async fn main() {
     // 1. Setup the container
-    _ = ServiceContainerBuilder::new().service(IdGenerator).build();
+    _ = ServiceContainerBuilder::new().build();
 
     // 2. Inject and call the function "make_sales_order"
     let new_sales_order = helpers::inject_and_call(make_sales_order).await;
-    println!("1. sales order created: {:#?}", new_sales_order);
+    println!("first sales order created: {:#?}", new_sales_order);
+
+    // 2. Inject and call the function "make_sales_order"
+    //    make_sales_order should receive the same IdGenerator instance
+    let new_sales_order = helpers::inject_and_call(make_sales_order).await;
+    println!("second sales order created: {:#?}", new_sales_order);
 }
 
-// 3. make_sales_order expect an instance of SalesOrder and the IdGenerator as a service
+// 3. make_sales_order expect an instance of SalesOrder and the IdGenerator as a singleton
 //   Note that the SalesOrder instance is not wrapped in RawType<T>, Singleton<T> or Service<T>.
 //   It is a plan old instance of the type
-async fn make_sales_order(mut so: SalesOrder, id_generator: Service<IdGenerator>) -> SalesOrder {
+async fn make_sales_order(mut so: SalesOrder, id_generator: Singleton<IdGenerator>) -> SalesOrder {
     so.id = id_generator.make();
 
     println!("generated ID for sales order {:#?}", so.id);
@@ -42,6 +47,14 @@ struct IdGenerator;
 impl IdGenerator {
     pub fn make(&self) -> String {
         uuid::Uuid::new_v4().to_string()
+    }
+}
+
+#[busybody::async_trait]
+impl busybody::Injectable for IdGenerator {
+    async fn inject(_: &busybody::ServiceContainer) -> Self {
+        println!("creating a new instance of the IdGenerator");
+        Self
     }
 }
 
