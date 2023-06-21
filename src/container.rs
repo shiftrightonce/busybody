1367f4682c7bef80500a11663772dcd5a69ae810
Jibao Mansaray <jibaomansaray@gmail.com>
2023-06-21 09:39:14 +0000

    Move most of the helper function to the container as methods

--- src/container.rs
+++ src/container.rs
@@ -67,6 +67,56 @@ impl ServiceContainer {
     pub fn set<T: Send + Sync + 'static>(&self, ext: T) -> &Self {
         self.set_type(Service::new(ext))
     }
+
+    /// Takes an async function or closure and executes it
+    /// Require arguments are injected during the call. All arguments must implement
+    /// Injectable.
+    ///
+    /// This method does not check for existing instance
+    pub async fn inject_and_call<F, Args>(&self, handler: F) -> F::Output
+    where
+        F: Handler<Args>,
+        Args: Injectable + 'static,
+    {
+        let args = Args::inject(self).await;
+        handler.call(args).await
+    }
+
+    /// Given a tuple of types, this method will try to resolve them
+    /// and return a tuple of instances
+    /// The types must implement Injectable.
+    ///
+    /// This method does not check for existing instance of the types.
+    pub async fn inject_all<Args>(&self) -> Args
+    where
+        Args: Injectable + 'static,
+    {
+        Args::inject(self).await
+    }
+
+    /// Given a type, this method will try to call the `inject` method
+    /// implemented on the type. It does not check the container for existing
+    /// instance.
+    pub async fn provide<T: Injectable + Send + Sync + 'static>(&self) -> T {
+        T::inject(self).await
+    }
+
+    /// Given a type, this method will try to find an instance of the type
+    /// wrapped in a `Service<T>` that is currently registered in the service
+    /// container.
+    pub async fn service<T: 'static>(&self) -> Service<T> {
+        Service::inject(self).await
+    }
+
+    /// Given a type, this method will try to find an existing instance of the
+    /// type. If that fails, an instance of the type is
+    /// initialized, wrapped in a `Service`, stored and
+    /// a copy is returned. Subsequent call requesting instance of that type will
+    /// returned. If the this is a proxy container, the instance will be dropped with
+    /// this container goes out of scope.
+    pub async fn singleton<T: Injectable + Sized + Send + Sync + 'static>(&self) -> Singleton<T> {
+        Singleton::inject(self).await
+    }
 }
 pub struct ServiceContainerBuilder {
     items: HashMap<TypeId, Box<dyn Any + Send + Sync + 'static>>,
