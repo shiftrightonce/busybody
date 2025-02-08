8794c5bf07fdd68bc9c93035ce66b18269d5b5dc
Jibao Mansaray <jibaomansaray@gmail.com>
2025-02-08 02:28:18 +0000

    example: remove RawType<T> from example

--- examples/inject_and_call.rs
+++ examples/inject_and_call.rs
@@ -1,4 +1,4 @@
-use busybody::{helpers, RawType, Service, ServiceContainerBuilder};
+use busybody::{helpers, Service, ServiceContainerBuilder};
 
 #[tokio::main]
 async fn main() {
@@ -14,15 +14,12 @@ async fn main() {
     let double_result = helpers::inject_and_call(double).await;
     println!("200 double is: {}", double_result);
 
-    // 3. Same as above but we are making use of "RawType<T>"
-    //    RawType<T> tries to find an instance of the specified type. If none exist,
-    //    it uses the `default` associate method to create a default instance of the Type.
-    //    This means, the "T" in RawType must implement the `Default` trait.
-    let sum = helpers::inject_and_call(|raw_i32: RawType<i32>, service_i32: Service<i32>| async {
-        raw_i32.into_inner() + *service_i32.into_inner()
+    // 3. Same as above but we are making use of a "raw" type and a Service
+    let sum = helpers::inject_and_call(|raw_i32: i32, service_i32: Service<i32>| async move {
+        raw_i32 + *service_i32
     })
     .await;
-    println!("Service<200> + RawType<400> = {}", sum);
+    println!("Service<200> + 400 = {}", sum);
 }
 
 // 4. Function is taken an I32.
