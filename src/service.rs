d0a95bbf320e7c067ba5243ba7d597a5043830f6
Jibao Mansaray <jibaomansaray@gmail.com>
2025-02-08 02:26:10 +0000

    fix(Service<T> and RawType<T>): both struct has been removed
    Service<T> was a thin wrapper around Arc<T>. There is no need
    for such indirecation. Arc<T> provides more features than Service<T>.
    
    RawType<T> existed at the time due to my skill issue.

--- src/service.rs
+++ src/service.rs
@@ -2,55 +2,9 @@
 
 use crate::{container::ServiceContainer, injectable::Injectable};
 use async_trait::async_trait;
-use std::{
-    ops::{Deref, DerefMut},
-    sync::Arc,
-};
+use std::sync::Arc;
 
-#[derive(Debug)]
-pub struct Service<T: ?Sized>(Arc<T>);
-
-impl<T> Service<T> {
-    pub fn new(the_service: T) -> Self {
-        Service(Arc::new(the_service))
-    }
-}
-
-impl<T: ?Sized> Service<T> {
-    pub fn get_ref(&self) -> &T {
-        self.0.as_ref()
-    }
-
-    pub fn into_inner(self) -> Arc<T> {
-        self.0
-    }
-}
-
-impl<T: ?Sized> Deref for Service<T> {
-    type Target = T;
-
-    fn deref(&self) -> &T {
-        self.0.as_ref()
-    }
-}
-
-impl<T: ?Sized> Clone for Service<T> {
-    fn clone(&self) -> Self {
-        Self(Arc::clone(&self.0))
-    }
-}
-
-impl<T: ?Sized> From<Arc<T>> for Service<T> {
-    fn from(arc: Arc<T>) -> Self {
-        Self(arc)
-    }
-}
-
-impl<T: Sized> From<T> for Service<T> {
-    fn from(arc: T) -> Self {
-        Self(Arc::new(arc))
-    }
-}
+pub type Service<T> = Arc<T>;
 
 #[async_trait]
 impl<T: 'static> Injectable for Service<T> {
@@ -60,55 +14,6 @@ impl<T: 'static> Injectable for Service<T> {
             None => {
                 panic!("Could not find service: {:?}", std::any::type_name::<T>())
             }
-        }
-    }
-}
-
-#[derive(Debug)]
-pub struct RawType<T: Clone>(T);
-
-impl<T: Clone> RawType<T> {
-    pub fn new(the_type: T) -> Self {
-        RawType(the_type)
-    }
-}
-
-impl<T: Clone> RawType<T> {
-    pub fn get_ref(&self) -> &T {
-        &self.0
-    }
-
-    pub fn into_inner(self) -> T {
-        self.0
-    }
-}
-
-impl<T: Clone> Deref for RawType<T> {
-    type Target = T;
-
-    fn deref(&self) -> &T {
-        &self.0
-    }
-}
-
-impl<T: Clone> DerefMut for RawType<T> {
-    fn deref_mut(&mut self) -> &mut Self::Target {
-        &mut self.0
-    }
-}
-
-impl<T: Clone> From<T> for RawType<T> {
-    fn from(the_type: T) -> Self {
-        Self(the_type)
-    }
-}
-
-#[async_trait]
-impl<T: Default + Clone + 'static> Injectable for RawType<T> {
-    async fn inject(container: &ServiceContainer) -> Self {
-        match container.get_type::<T>() {
-            Some(service) => Self::new(service),
-            None => Self::new(T::default()),
         }
     }
 }
@@ -133,11 +38,5 @@ mod test {
     async fn test_creating_from_t() {
         let service: Service<i32> = 9000.into();
         assert_eq!(*service, 9000);
-    }
-
-    #[tokio::test]
-    async fn test_raw_type() {
-        let var: RawType<String> = "Hello World".to_string().into();
-        assert_eq!(var.as_str(), "Hello World");
     }
 }
